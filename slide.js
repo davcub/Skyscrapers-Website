be9c6b2ac77a71a182b4a7d9cf82c96217d353fa
David Cubrilla <davidcubrilla@gmail.com>
2020-12-06 12:01:21 -0500

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge"> <!-- For intranet testing only, remove in production. -->
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <title>Slide Show</title>
+</head>
+
+<body>
+<div id="slideshow">
+	<div><img src="homepic.jpg" alt="Home" /></div>	
+	<div><img src="p4pic1.jpg" alt="Home" /></div>	
+	<div><img src="p4pic2.jpg" alt="Home" /></div>	
+	<div><img src="p4pic3.jpg" alt="Home" /></div>	
+	<div><img src="p4pic4.jpg" alt="Home" /></div>	
+</div> 
+  <button id="slideShowButton"></button> <!-- Optional button element. -->
+  <script src="slideShow.js"></script>
+</body>
+
+</html>
